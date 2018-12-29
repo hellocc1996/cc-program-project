320262e72aaefa93e5f58a8c437b2abab50404ed
cuipp <1107235099@qq.com>
2018-12-29 16:23:43 +0800

    xml、java操作工具类新增

--- common/cc-common-utils/src/main/java/com/program/util/xml/JavaToXmlUtils.java
+++ common/cc-common-utils/src/main/java/com/program/util/xml/JavaToXmlUtils.java
@@ -1,17 +1,26 @@
 package com.program.util.xml;
 
+import com.program.util.exception.XmlException;
 import org.w3c.dom.Element;
+
+import java.lang.reflect.Field;
 
 public class JavaToXmlUtils {
 
     /**
      * @param parent
-     * @param fields
+     * @param obj
      * @Desc 把实体类中有的字段直接转为xml节点
      */
-    public static void entityToXml(Element parent, String... fields) {
-        for (String field : fields) {
-            XmlUtils.appendElement(parent, field, "取值");
+    public static void entityToXml(Element parent, Object obj) {
+        Class cls = obj.getClass();
+        Field[] fields = cls.getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                XmlUtils.appendElement(parent, field.getName(), field.get(obj).toString());
+            }
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            throw new XmlException(XmlException.XML_PARSE_ERROR, e);
         }
     }
 }
